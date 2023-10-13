f34a8618719ddd06404f8d6b7153b3578dfde3ff
VincentWinkelmuller <v.winkelmuller@gmail.com>
2023-10-13 13:36:57 +0200

    Initial App commit

---
+++ BalanceControlVW/BalanceControlVW/BalanceControlVWApp.swift
@@ -0,0 +1,17 @@
+//
+//  BalanceControlVWApp.swift
+//  BalanceControlVW
+//
+//  Created by Winkelmuller on 13/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct BalanceControlVWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
